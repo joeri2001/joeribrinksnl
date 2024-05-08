7069d418be89d93e7c0691e7725f816bc1ef0e76
joeri2001 <joeribrinks68@gmail.com>
2024-05-09 01:44:49 +0200

    add error handling, and fallback to homepage if post doesn't exist

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -2,13 +2,18 @@ import fs from "fs";
 import Markdown from "markdown-to-jsx";
 import matter from "gray-matter";
 import getPostMetadata from "@/components/blog/getPostMetadata";
+import Link from "next/link";
 
 const getPostContent = (slug: string) => {
   const folder = "posts/";
   const file = `${folder}${slug}.md`;
-  const content = fs.readFileSync(file, "utf8");
-  const matterResult = matter(content);
-  return matterResult;
+  try {
+    const content = fs.readFileSync(file, "utf8");
+    const matterResult = matter(content);
+    return matterResult;
+  } catch (error) {
+    return null;
+  }
 };
 
 export const generateStaticParams = async () => {
@@ -21,6 +26,16 @@ export const generateStaticParams = async () => {
 const PostPage = (props: any) => {
   const slug = props.params.slug;
   const post = getPostContent(slug);
+
+  if (!post) {
+    return (
+      <div>
+        <p>Post not found!</p>
+        <Link href={"/"}>Go back to home</Link>
+      </div>
+    );
+  }
+
   return (
     <div>
       <div>
